1bd0372fcd66f1b859fb3c2391d2f1008b0d0038
Recitorex <143562580+Recitorex@users.noreply.github.com>
2023-09-30 17:25:12 -0600

    Add files via upload

---
+++ FP-EXA-1-EVA-1-E1/src/main/java/com/mycompany/fp/exa/eva/e1/FPEXA1EVA1E1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fp.exa.eva.e1;
+
+/**
+ *
+ * @author mayen
+ */
+public class FPEXA1EVA1E1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nombre del equipo:");
+        System.out.println("CUPCAKE");
+        System.out.println("Nombre del proyecto:");
+        System.out.println("Lucky Words");
+        System.out.println("Nombre de cada integrante del equipo y Numeros de control:");
+        System.out.println("ALBERTO JEREMOTH MAYEN PALMA – 23550327  ");
+        System.out.println("CARLOS ALBERTO CERDA ALTAMIRANO – 23550348 ");
+        System.out.println("ANGEL RICARDO AGUILAR PONCE DE LEON – 23550833 ");
+        System.out.println("DANIEL ISAAC GANDARA TAPIA – 23550374 ");
+       
+    
+    }
+}
