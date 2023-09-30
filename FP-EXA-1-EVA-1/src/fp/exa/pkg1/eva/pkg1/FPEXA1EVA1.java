2f37d3184bbb81d6adc326d4e748f48ef1554fae
Carlos Cerda <143562904+futuroinge@users.noreply.github.com>
2023-09-30 12:44:55 -0600

    Update FPEXA1EVA1.java

--- FP-EXA-1-EVA-1/src/fp/exa/pkg1/eva/pkg1/FPEXA1EVA1.java
+++ FP-EXA-1-EVA-1/src/fp/exa/pkg1/eva/pkg1/FPEXA1EVA1.java
@@ -14,7 +14,7 @@ public class FPEXA1EVA1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Equipo Cupcake")
     }
     
 }
